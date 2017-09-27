10be31901c4b82d8d214039da8c4183b80b31940
jge162 <31228460+jge162@users.noreply.github.com>
2017-09-27 16:33:12 -0700

    Add files via upload

--- PriceListItem.h
+++ PriceListItem.h
@@ -1,18 +1,23 @@
+//PriceListItem.h
+
 #pragma once
+
+#include<iostream>
 #include <string>
-
 using namespace std;
-
 class PriceListItem {
-public:
-	PriceListItem();
-	PriceListItem(const string &itemName, const string &code, double price, bool taxable);
-	string getItemName();
-	string getCode();
-	double getPrice();
-	bool isTaxable();
 private:
-	// any private member variables and methods go here
-	// TO BE COMPLETED
-
+    // private member variables
+    string itemName;
+    string code;
+    double price;
+    bool   taxable;
+public:
+    PriceListItem();
+    PriceListItem(const string &itemName, const string &code, double price, bool taxable);
+    string getItemName();
+    string getCode();
+    double getPrice();
+    bool isTaxable();
+    
 };
