c3c8d281350e35dd9b2e53cceff2521042b0d09b
jge162 <31228460+jge162@users.noreply.github.com>
2017-09-27 22:18:32 -0700

    Updated PriceList.cpp

--- PriceList.cpp
+++ PriceList.cpp
@@ -1,44 +1,83 @@
+//
+//  PriceList.cpp
+//  Project - CPSC - 131 -05
+//
+//  Created by Jeremy Escobar on 9/27/17.
+//  Copyright © 2017 Jeremy Escobar. All rights reserved.
+//
+
+
 #include <string>
 #include <iostream>
 #include <fstream>
 #include <stdexcept>
+
 #include "PriceList.h"
 #include "PriceListItem.h"
 
 using namespace std;
 
-// Load information from a text file with the given filename.
-void PriceList::createPriceListFromDatafile(string filename) {
-	ifstream myfile(filename);
-
-	if (myfile.is_open()) {
-		cout << "Successfully opened file " << filename << endl;
-		string name;
-		string code;
-		double price;
-		bool taxable;
-		while (myfile >> name >> code >> price >> taxable) {
-			// cout << code << " " << taxable << endl;
-			addEntry(name, code, price, taxable);
-		}
-		myfile.close();
-	}
-	else
-		throw invalid_argument("Could not open file " + filename);
+PriceList::PriceList()
+{
+    futureItems = new PriceListItem[1000000];
+    itemSize    = 0;
 }
 
-// return true only if the code is valid
-bool PriceList::isValid(string code) const {
-	// TO BE COMPLETED
+// Load information from a text file with the given filename.
 
+void PriceList::createPriceListFromDatafile(string filename) {
+    ifstream myfile(filename);
+    
+    if (myfile.is_open()) {
+        cout << "Successfully opened file " << filename << endl;
+        string name;
+        string code;
+        double price;
+        bool taxable;
+            while (myfile >> name >> code >> price >> taxable) {
+            //cout << code << " " << taxable << endl;
+                addEntry (name, code, price, taxable);
+                    }
+                        myfile.close();
+                            }
+    else
+        throw invalid_argument("Could not open file " + filename);
+}
+// return true only if the code is valid
+
+bool PriceList::isValid(string code) const {
+    
+    for ( int i = 0; i < itemSize; i++)
+    {
+        if ( futureItems[i].getCode() == code)
+            return true;
+                }
+                    return false;
 }
 
 // return price, item name, taxable? as an ItemPrice object; throw exception if code is not found
+
 PriceListItem PriceList::getItem(string code) const {
-	// TO BE COMPLETED
+    
+    for (int j = 0; j < itemSize; j++)
+    {
+        if ( futureItems[j].getCode() == code)
+            return futureItems[j];
+                }
+                    throw invalid_argument("Invalid code: " + code);
 }
 
 // add to the price list information about a new item
+
 void PriceList::addEntry(string itemName, string code, double price, bool taxable) {
-	// TO BE COMPLETED
+    
+    
+    if ( itemSize == 0)
+        {
+            futureItems = new PriceListItem[1000000];
+                }
+                    PriceListItem pricedItems = PriceListItem (itemName, code, price, taxable);
+                        futureItems [itemSize] = pricedItems;
+                            itemSize++;
+    
 }
