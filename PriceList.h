88c6d6fc52ef39631cc2b8c852cbdc3e8ab316d3
jge162 <31228460+jge162@users.noreply.github.com>
2017-09-27 22:17:59 -0700

    Updated PriceList.h

--- PriceList.h
+++ PriceList.h
@@ -1,22 +1,37 @@
+//
+//  PriceList.h
+//  Project - CPSC - 131 -05
+//
+//  Created by Jeremy Escobar on 9/27/17.
+//  Copyright © 2017 Jeremy Escobar. All rights reserved.
+//
+
 #pragma once
+
 #include <string>
 #include <stdexcept>
+
 #include "PriceListItem.h"
 
 using namespace std;
 
 class PriceList {
-public:
-	
-	void createPriceListFromDatafile(string filename); // Load information from a text file with the given filename (Completed)
-	void addEntry(string itemName, string code, double price, bool taxable); // add to the price list information about a new item. A max of 1,000,000 entries can be added
-	bool isValid(string code) const; // return true only if the code is valid
-	PriceListItem getItem(string code) const; // return price, item name, taxable? as an PriceListItem object; throw exception if code is not found
-
+    
 private:
-	// Add private member variables for your class along with any 
-	// other variables required to implement the public member functions
-	// TO BE COMPLETED
-
-
+    
+    PriceListItem *futureItems;
+    int itemSize;
+    
+public:
+    
+    PriceList();
+    //default constructor
+    void createPriceListFromDatafile(string filename);
+    // Load information from a text file with the given filename (Completed)
+    void addEntry(string itemName, string code, double price, bool taxable);
+    // add to the price list information about a new item. A max of 1,000,000 entries can be added
+    bool isValid(string code) const;
+    // return true only if the code is valid
+    PriceListItem getItem(string code) const;
+    // return price, item name, taxable? as an PriceListItem object; throw exception if code is not found
 };
