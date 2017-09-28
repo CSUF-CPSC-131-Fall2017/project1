abf8b063ba16b75b0a7e22ab937de5712aef0540
jge162 <31228460+jge162@users.noreply.github.com>
2017-09-27 22:14:59 -0700

    Updated GroceryBill.cpp

--- GroceryBill.cpp
+++ GroceryBill.cpp
@@ -1,43 +1,117 @@
-#include "GroceryBill.h"
+//
+//  GroceryBill.cpp
+//  Project - CPSC - 131 -05
+//
+//  Created by Jeremy Escobar on 9/27/17.
+//  Copyright © 2017 Jeremy Escobar. All rights reserved.
+//
+
+#include <fstream>
 #include <iostream>
+
+#include "GroceryBill.h"
 
 using namespace std;
 
 GroceryBill::GroceryBill(const PriceList *priceList, double taxRate) {
-	// To be completed
-
+    groceryBillTotal      = 0;
+    billCount         = 0;
+    theCodesAre       = new string [1000000];
+    quanity           = new double [1000000];
+    this->listOfItems = *priceList;
+    this->taxRate     = taxRate;
 }
 
-
 void GroceryBill::scanItem(string scanCode, double quantity) {
-	// To be completed
+    
+    PriceListItem items = listOfItems.getItem (scanCode);
+    
+        groceryBillTotal += items.getPrice()*quantity;
+    
+    if (items.isTaxable())
+        
+                groceryBillTotal += items.getPrice()*quantity*taxRate;
+    
+                    theCodesAre[billCount] = scanCode;
+    
+                        quanity[billCount++] = quantity;
+    
 }
 
 // Scan multiple codes and quantities from the given text file
+
 // Each line contains two numbers separated by space: the first is the code (an integer), the second the quantity (a float/double)
+
 // Example line from text file:
+
 // 15000000 1.5
+
 void GroceryBill::scanItemsFromFile(string filename) {
-	// To be completed
-	// HINT: Look at code in PriceList::createPriceListFromDatafile(string filename)
+    
+    // To be completed
+    
+    // HINT: Look at code in PriceList::createPriceListFromDatafile(string filename)
+    
+    ifstream myfile(filename);
+    
+    if (myfile.is_open()) {
+        
+        cout << "Successfully opened file " << filename << endl;
+        string code;
+        double qty;
+        while (myfile >> code >> qty) {
+            scanItem (code, qty);
+        }
+            myfile.close();
+        
+    }
+    
+    else
+        
+        throw invalid_argument("Could not open file " + filename);
+    
 }
 
 // return the total cost of all items scanned
+
 double GroceryBill::getTotal() {
-	// To be completed
+    
+    return groceryBillTotal;
+    
 }
 
-// Print the bill to cout. Each line contains the name of an item, total price, and the letter "T" if tax was addded. 
+// Print the bill to cout. Each line contains the name of an item, total price, and the letter "T" if tax was addded.
+
 // The last line shows the total.
+
 // An example:
-//Plastic_Wrap	1.60547 T
-//Sugar_white	5.475
-//Waffles_frozen	5.16
-//Oil_Canola_100%_pure	2.69
-//Potatoes_red	13.446
-//TOTAL 	28.3765
+
+//Plastic_Wrap 1.60547 T
+
+//Sugar_white 5.475
+
+//Waffles_frozen 5.16
+
+//Oil_Canola_100%_pure 2.69
+
+//Potatoes_red 13.446
+
+//TOTAL 28.3765
+
 void GroceryBill::printBill() {
-	// To be completed
-
+    
+    for(int i = 0; i < billCount; i++)
+        
+    {
+        
+        cout << listOfItems.getItem(theCodesAre[i]).getItemName()<< " "<<quanity[i] << " ";
+        
+            if (listOfItems.getItem(theCodesAre[i]).isTaxable())
+            
+                cout << "T";
+        
+                    cout << endl;
+        
+                        }
+    
 }
-
