236235399b66f9a28da647714458fafbd52f9588
jge162 <31228460+jge162@users.noreply.github.com>
2017-09-27 22:17:12 -0700

    Updated PriceListItem.cpp

--- PriceListItem.cpp
+++ PriceListItem.cpp
@@ -1,26 +1,33 @@
-//PriceListItem cpp.
+//
+//  PriceListItem.cpp
+//  Project - CPSC - 131 -05
+//
+//  Created by Jeremy Escobar on 9/27/17.
+//  Copyright © 2017 Jeremy Escobar. All rights reserved.
+//
+
 #include<iostream>
 #include<string>
+
 #include "PriceListItem.h"
 using namespace std;
 
 //paramter constructor
 PriceListItem::PriceListItem(const string &itemName, const string &code, double price, bool taxable) {
-    this->itemName =itemName;
-    this->code     =code;
-    this->price    =price;
-    this->taxable  =taxable;
+    this->itemName = itemName;
+    this->code     = code;
+    this->price    = price;
+    this->taxable  = taxable;
     
 }
 //default constructor
 PriceListItem::PriceListItem() {
-    itemName="";
-    code="";
-    price=0;
-    taxable=false;
+    itemName = "";
+    code     = "";
+    price    = 0;
+    taxable  = false;
 }
-string PriceListItem::getItemName() { return itemName; }
-string PriceListItem::getCode()     { return code;}
-double PriceListItem::getPrice()    { return price;}
-bool PriceListItem::isTaxable()     { return taxable;}
-
+string PriceListItem::getItemName() { return itemName;}
+string PriceListItem::getCode()     { return code;    }
+double PriceListItem::getPrice()    { return price;   }
+bool   PriceListItem::isTaxable()   { return taxable; }
