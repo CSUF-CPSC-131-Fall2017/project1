1ea7bdae616ccc96474d24d701db770fc78d9a4e
jge162 <31228460+jge162@users.noreply.github.com>
2017-09-27 22:16:21 -0700

    Updated PriceListItem.h

--- PriceListItem.h
+++ PriceListItem.h
@@ -1,9 +1,16 @@
-//PriceListItem.h
+//
+//  PriceListItem.h
+//  Project - CPSC - 131 -05
+//
+//  Created by Jeremy Escobar on 9/27/17.
+//  Copyright © 2017 Jeremy Escobar. All rights reserved.
+//
 
 #pragma once
 
 #include<iostream>
 #include <string>
+
 using namespace std;
 class PriceListItem {
 private:
@@ -12,12 +19,14 @@ private:
     string code;
     double price;
     bool   taxable;
+    
 public:
-    PriceListItem();
+    
+    PriceListItem(); 
     PriceListItem(const string &itemName, const string &code, double price, bool taxable);
     string getItemName();
     string getCode();
     double getPrice();
-    bool isTaxable();
+    bool   isTaxable();
     
 };
